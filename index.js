b032b9fddda1ca943a4fbf153a9a5469ca872104
srbharath <103111244+srbharath@users.noreply.github.com>
2023-05-10 15:07:57 +0530

    Update index.js-etcd

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('rootpath')();
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -17,18 +18,36 @@ app.use(errorHandler);
 
 const Etcd3 = require('etcd3');
 
-// create a new etcd client object
-const client = new Etcd3({
-  hosts: 'etcd:2379', // replace with the actual etcd cluster endpoint
-});
-// fetch the value of the 'config' key from etcd
-client.get('config').string().then((value) => {
-  // do something with the configuration value
-  console.log('Configuration value:', value);
-}).catch((error) => {
-  console.error('Error fetching configuration:', error);
-});
+const config = {
+  key1: 'config/file',
+  //key2: 'value2',
+  // add more key-value pairs as needed
+};
 
-// start server
+function pushConfigToEtcd(config) {
+  const client = new Etcd3({
+    hosts: 'etcd:2379', // replace with the actual etcd cluster endpoint
+  });
+
+  client.put('config/file', JSON.stringify(config))
+    .then(() => console.log('Configuration file pushed successfully'))
+    .catch((err) => console.error('Error pushing configuration file:', err));
+
+  const watcher = client.watch('config/file');
+  watcher.on('data', (res) => {
+    const config = JSON.parse(res.toString());
+    console.log('New configuration file:', config);
+  });
+
+  client.get('config/file')
+    .json()
+    .then(config => {
+      console.log(config);
+    })
+    .catch((err) => console.error('Error getting configuration file:', err));
+}
+
+pushConfigToEtcd(config); // call the function with your configuration object
+
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 app.listen(port, () => console.log('Server listening on port ' + port));
